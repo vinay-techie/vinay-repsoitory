dd79213dc39b32cd7d9b82afd3ffe29df3bea760
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2019-11-29 17:45:12 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function fward(){
+	var counter=1
+	document.getElementById("main").innerHTML=attr(src,getElementById("img"+counter))
+	counter=counter+1
+	document.getElementById("main").innerHTML=attr(src,getElementById("img"+counter))
+
+}
