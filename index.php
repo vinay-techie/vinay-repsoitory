6070e617501f107cebee53611a63a5cef55eb066
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-01-07 15:55:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,11 +29,7 @@ $sql="CREATE TABLE student(
       )";
 $conn->exec($sql);
 echo "table created";
-$y = $conn->query("SELECT * FROM student WHERE ROLL='$_POST[data]'");
-while($row = $y->fetch(PDO::FETCH_ASSOC)){
-   echo "ROLL NUMBER:".$row[ROLL]."</br>";
-   echo "NAME:".$row[name]."</br>";
-}
+
 echo "</pre>\n";
    
 ?>
