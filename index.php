a748565f9167e2f6fc9c7faca4c7f9b5a64a808d
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-01-07 15:18:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
 <body style='background-color:#E0FFFF;opacity:0.5'>
-<h1 style='position:relative;left:39%'> CSE-B</h1>
+<h1 style='position:relative;left:44%'> CSE-B</h1>
 </body>
 <fieldset style='display:inline;position:relative;left:39%;border-radius:10px'>
 <form  method='post'>
